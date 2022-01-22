b6d5e9100f7b04a2e8a12bca939058c285a2d90a
mooncat <mooncat@MOONCAT>
2022-01-22 23:00:29 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtnama = New System.Windows.Forms.TextBox()
+        Me.txtnim = New System.Windows.Forms.TextBox()
+        Me.txtnilai = New System.Windows.Forms.TextBox()
+        Me.txtgrade = New System.Windows.Forms.TextBox()
+        Me.btnhitung = New System.Windows.Forms.Button()
+        Me.btnhapus = New System.Windows.Forms.Button()
+        Me.btnkeluar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(90, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Menghitung grade nilai"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 157)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(27, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nilai"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 207)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Grade"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 83)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Nama"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(36, 117)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(27, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "NIM"
+        '
+        'txtnama
+        '
+        Me.txtnama.Location = New System.Drawing.Point(93, 75)
+        Me.txtnama.Name = "txtnama"
+        Me.txtnama.Size = New System.Drawing.Size(196, 20)
+        Me.txtnama.TabIndex = 5
+        '
+        'txtnim
+        '
+        Me.txtnim.Location = New System.Drawing.Point(93, 114)
+        Me.txtnim.Name = "txtnim"
+        Me.txtnim.Size = New System.Drawing.Size(131, 20)
+        Me.txtnim.TabIndex = 6
+        '
+        'txtnilai
+        '
+        Me.txtnilai.Location = New System.Drawing.Point(93, 153)
+        Me.txtnilai.Name = "txtnilai"
+        Me.txtnilai.Size = New System.Drawing.Size(121, 20)
+        Me.txtnilai.TabIndex = 7
+        '
+        'txtgrade
+        '
+        Me.txtgrade.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtgrade.Location = New System.Drawing.Point(93, 207)
+        Me.txtgrade.Multiline = True
+        Me.txtgrade.Name = "txtgrade"
+        Me.txtgrade.ReadOnly = True
+        Me.txtgrade.Size = New System.Drawing.Size(152, 68)
+        Me.txtgrade.TabIndex = 8
+        Me.txtgrade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnhitung
+        '
+        Me.btnhitung.Location = New System.Drawing.Point(40, 322)
+        Me.btnhitung.Name = "btnhitung"
+        Me.btnhitung.Size = New System.Drawing.Size(75, 119)
+        Me.btnhitung.TabIndex = 9
+        Me.btnhitung.Text = "Hitung"
+        Me.btnhitung.UseVisualStyleBackColor = True
+        '
+        'btnhapus
+        '
+        Me.btnhapus.Location = New System.Drawing.Point(238, 321)
+        Me.btnhapus.Name = "btnhapus"
+        Me.btnhapus.Size = New System.Drawing.Size(75, 41)
+        Me.btnhapus.TabIndex = 10
+        Me.btnhapus.Text = "Hapus"
+        Me.btnhapus.UseVisualStyleBackColor = True
+        '
+        'btnkeluar
+        '
+        Me.btnkeluar.Location = New System.Drawing.Point(238, 409)
+        Me.btnkeluar.Name = "btnkeluar"
+        Me.btnkeluar.Size = New System.Drawing.Size(75, 32)
+        Me.btnkeluar.TabIndex = 11
+        Me.btnkeluar.Text = "Keluar"
+        Me.btnkeluar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(336, 472)
+        Me.Controls.Add(Me.btnkeluar)
+        Me.Controls.Add(Me.btnhapus)
+        Me.Controls.Add(Me.btnhitung)
+        Me.Controls.Add(Me.txtgrade)
+        Me.Controls.Add(Me.txtnilai)
+        Me.Controls.Add(Me.txtnim)
+        Me.Controls.Add(Me.txtnama)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtnama As TextBox
+    Friend WithEvents txtnim As TextBox
+    Friend WithEvents txtnilai As TextBox
+    Friend WithEvents txtgrade As TextBox
+    Friend WithEvents btnhitung As Button
+    Friend WithEvents btnhapus As Button
+    Friend WithEvents btnkeluar As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub btnhitung_Click(sender As Object, e As EventArgs) Handles btnhitung.Click
+        Dim nama As String = txtnama.Text
+        Dim nim As String = txtnim.Text
+        Dim nilai As String = txtnilai.Text
+        Dim grade As String = txtgrade.Text
+
+        If nama = "" Then
+            MessageBox.Show("Anda tidak memasukan Nama.", "Perhatian", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            If nim = "" Then
+                MessageBox.Show("Anda tidak memasukan NIM.", "Perhatian", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                If nilai = "" Then
+                    MessageBox.Show("Anda tidak memasukan Nilai", "Perhatian", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Else
+                    If nilai >= 80 And (nilai <= 100) Then
+                        grade = "A"
+                    ElseIf nilai >= 70 And (nilai <= 79) Then
+                        grade = "B"
+                    ElseIf nilai >= 60 And (nilai <= 69) Then
+                        grade = "C"
+                    ElseIf nilai >= 50 And (nilai <= 59) Then
+                        grade = "D"
+                    ElseIf nilai >= 0 And (nilai <= 49) Then
+                        grade = "E"
+                    Else
+                        grade = "Wrong"
+                    End If
+                End If
+            End If
+        End If
+        txtgrade.Text = grade
+    End Sub
+
+    Private Sub btnhapus_Click(sender As Object, e As EventArgs) Handles btnhapus.Click
+        txtgrade.Clear()
+        txtnama.Clear()
+        txtnilai.Clear()
+        txtnim.Clear()
+    End Sub
+
+    Private Sub btnkeluar_Click(sender As Object, e As EventArgs) Handles btnkeluar.Click
+        End
+    End Sub
+End Class
